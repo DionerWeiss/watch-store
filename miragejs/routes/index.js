b98d0a620e4ebf27b79d12a885d44a9b9dd47ceb
Dioner Weiss <dionerweiss@gmail.com>
2022-05-04 23:12:00 -0300

    create mirage starter kit

---
+++ miragejs/routes/index.js
@@ -0,0 +1,13 @@
+export default function routes() {
+  this.namespace = 'api';
+  this.resource('users');
+  this.resource('products');
+
+  this.get('messages', (schema, request) => {
+    const {
+      queryParams: { userId },
+    } = request;
+
+    return schema.messages.where({ userId });
+  });
+}
