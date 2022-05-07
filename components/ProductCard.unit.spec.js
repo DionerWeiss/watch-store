6bfc980025db25c86f6f9aa2da9e436b5a52a703
Dioner Weiss <dionerweiss@gmail.com>
2022-05-07 00:03:27 -0300

    test: emit the event addToCart with product object when button gets clicked

--- components/ProductCard.unit.spec.js
+++ components/ProductCard.unit.spec.js
@@ -2,17 +2,22 @@ import { mount } from '@vue/test-utils';
 import ProductCard from '@/components/ProductCard';
 import { makeServer } from '@/miragejs/server';
 
-const mountProductCard = (server) =>
-  mount(ProductCard, {
-    propsData: {
-      product: server.create('product', {
-        title: 'Relógio Bonito',
-        price: '22.00',
-        image:
-          'https://images.unsplash.com/photo-1526045431048-f857369baa09?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-      }),
-    },
+const mountProductCard = (server) => {
+  const product = server.create('product', {
+    title: 'Relógio Bonito',
+    price: '22.00',
+    image:
+      'https://images.unsplash.com/photo-1526045431048-f857369baa09?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
   });
+  return {
+    wrapper: mount(ProductCard, {
+      propsData: {
+        product,
+      },
+    }),
+    product,
+  };
+};
 
 describe('ProductCard - unit', () => {
   let server;
@@ -26,16 +31,26 @@ describe('ProductCard - unit', () => {
   });
 
   it('should match snapshot', () => {
-    const wrapper = mountProductCard(server);
+    const { wrapper } = mountProductCard(server);
 
     expect(wrapper.element).toMatchSnapshot();
   });
 
   it('should mount the component', () => {
-    const wrapper = mountProductCard(server);
+    const { wrapper } = mountProductCard(server);
 
     expect(wrapper.vm).toBeDefined();
     expect(wrapper.text()).toContain('Relógio Bonito');
     expect(wrapper.text()).toContain('$22.00');
+  });
+
+  it('should emit the event addToCart with product object when button gets clicked', async () => {
+    const { wrapper, product } = mountProductCard(server);
+
+    await wrapper.find('button').trigger('click');
+
+    expect(wrapper.emitted().addToCart).toBeTruthy();
+    expect(wrapper.emitted().addToCart.length).toBe(1);
+    expect(wrapper.emitted().addToCart[0]).toEqual([{ product }]);
   });
 });
