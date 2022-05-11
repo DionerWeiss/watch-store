e131566b13667c516ec78468524a015dde1b443d
Dioner Weiss <dionerweiss@gmail.com>
2022-05-10 22:37:16 -0300

    test:  filter the product list when a search is performed

--- pages/ProductList.integration.spec.js
+++ pages/ProductList.integration.spec.js
@@ -71,4 +71,34 @@ describe('Name of the group', () => {
 
     expect(wrapper.text()).toContain('Problemas ao carregar a lista!');
   });
+
+  it('should filter the product list when a search is performed', async () => {
+    const products = [
+      ...server.createList('product', 10),
+      server.create('product', {
+        title: 'Meu relógio amado',
+      }),
+      server.create('product', {
+        title: 'Meu outro relógio estimado',
+      }),
+    ];
+
+    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+
+    await Vue.nextTick();
+
+    const search = wrapper.findComponent(Search);
+    search.find('input[type="search"]').setValue('relógio');
+    await search.find('form').trigger('submit');
+
+    const cards = wrapper.findAllComponents(ProductCard);
+    // expect(wrapper.vm.searchTerm).Equal('relógio');
+    expect(cards).toHaveLength(2);
+  });
 });
