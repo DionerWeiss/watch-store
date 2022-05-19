0c4f9c1480077f43aa4f1aca0a440bbd41282581
Dioner Weiss <dionerweiss@gmail.com>
2022-05-18 22:11:20 -0300

    feat: not go bellow zero when button - is repeatedly clicked

--- components/CartItem.vue
+++ components/CartItem.vue
@@ -32,7 +32,7 @@
           <button
             class="text-gray-500 focus:outline-none focus:text-gray-600"
             data-testid="-"
-            @click="quantity--"
+            @click="decrease"
           >
             <svg
               class="h-5 w-5"
@@ -68,6 +68,13 @@ export default {
     return {
       quantity: 1,
     };
+  },
+  methods: {
+    decrease() {
+      if (this.quantity > 0) {
+        this.quantity--;
+      }
+    },
   },
 };
 </script>
